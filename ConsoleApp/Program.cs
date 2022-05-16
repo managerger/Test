2bf59df1ac632f23805d2e2fba66b290cbda4a06
managerger <konstantynaleksandrov111@gmail.com>
2022-05-16 12:42:22 +0300

    Initial

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using MSProject;
+using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var msProjectAnalyzer = new MSProjectAnalyzer();
+            var currentProjectDir = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            var projectToTest = Path.Combine(currentProjectDir, "ConsoleApp.csproj");
+            var projectFilesList = msProjectAnalyzer.GetProjectItems(projectToTest);
+        }
+    }
+}
---
+++ MSProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using Buildalyzer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MSProject
+{
+    public class MSProjectAnalyzer
+    {
+        private readonly IEnumerable<string> _projectItemTypes = new[] { "ClCompile", "ClInclude", "ResourceCompile", "Compile", "EmbeddedResource", "Content", "None" };
+
+        public List<string> GetProjectItems(string projectFilePath)
+        {
+            AnalyzerManager manager = new AnalyzerManager();
+            IProjectAnalyzer analyzer = manager.GetProject(projectFilePath);
+            IAnalyzerResults results = analyzer.Build();
+            IAnalyzerResult result = results.First();
+
+            List<string> projectItemTypes = new List<string>();
+            var projectItemFiles = result.Items.Where(i => _projectItemTypes.Contains(i.Key)).SelectMany(i => i.Value).Select(i => Path.Combine(analyzer.SolutionDirectory, i.ItemSpec)).ToList();
+
+            return projectItemFiles;
+        }
+    }
+}
